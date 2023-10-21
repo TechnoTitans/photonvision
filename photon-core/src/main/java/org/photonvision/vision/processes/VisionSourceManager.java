26e42d154e5891b15fb26921df595c7d57c64183
MqxS <maxgoog06@outlook.com>
2023-10-21 10:58:17 -0400

    A few more changes, mainly just to camera setup.

--- photon-core/src/main/java/org/photonvision/vision/processes/VisionSourceManager.java
+++ photon-core/src/main/java/org/photonvision/vision/processes/VisionSourceManager.java
@@ -77,9 +77,9 @@ public class VisionSourceManager {
             () -> List.of(
 //                    UsbCamera.enumerateUsbCameras()
                     new UsbCameraInfo(0, "/dev/cameras/FL_Apriltag_L", "FL_Apriltag_L", new String[0],  0x0edc, 0x2076),
-                    //new UsbCameraInfo(0, "/dev/cameras/FR_Apriltag_F", "FR_Apriltag_F", new String[0],  0x0edc, 0x2076),
-                    new UsbCameraInfo(0, "/dev/cameras/BL_Apriltag_B", "BL_Apriltag_B", new String[0],  0x2560, 0xc128)
-                    //new UsbCameraInfo(0, "/dev/cameras/FL_Apriltag_L", "FL_Apriltag_L", new String[0],  0x0edc, 0x2076),
+                    new UsbCameraInfo(1, "/dev/cameras/FR_Apriltag_F", "FR_Apriltag_F", new String[0],  0x045e, 0x0810),
+                    new UsbCameraInfo(2, "/dev/cameras/BL_Apriltag_B", "BL_Apriltag_B", new String[0],  0x2560, 0xc128),
+                    new UsbCameraInfo(3, "/dev/cameras/drivercam1", "drivercam1", new String[0],  0x0edc, 0x2076)
             );
 
     protected void tryMatchUSBCams() {
